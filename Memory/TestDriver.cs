5594c770a2b990bc6334d0c059c08ed2417dfb6b
MonteFloyd <41308639+MonteFloyd@users.noreply.github.com>
2018-07-23 03:58:52 +0300

    Added stackalloc tests

---
+++ Memory/TestDriver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace DotNetBenchmark
+{
+
+
+    class TestDriver
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            BenchmarkRunner.Run<stackAllocTest>();
+
+        }
+    }
+}
---
+++ Memory/stackAllocTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace DotNetBenchmark
+{
+
+    [MemoryDiagnoser]
+    public class stackAllocTest
+    {
+        public int[] inputArray;
+
+        public System.Random random;
+
+        public stackAllocTest()
+        {
+            inputArray = new int[10000];
+            random = new Random();
+            for(int i = 0; i< inputArray.Length; ++i)
+            {
+                inputArray[i] = (random.Next());
+            }
+
+        }
+
+
+
+        [Benchmark]
+        public void normalOp()
+        {
+            //Allocate the array
+            int[] copyArray = new int[inputArray.Length];
+
+
+            //Sample the original array
+            for (int i = 0; i < copyArray.Length; ++i)
+            {
+                copyArray[i] = inputArray[i];
+            }
+
+            //Do operations on the sampled array
+            for (int i = 0; i < copyArray.Length; ++i)
+            {
+                ++copyArray[i];
+            }
+
+            //Use the modifications
+            for (int i = 0; i < copyArray.Length; ++i)
+            {
+                inputArray[i] = copyArray[i];
+            }
+
+        }
+
+        [Benchmark]
+        public void stackAllocOp()
+        {
+            //Allocate the array on stack
+            Span<int> copyArray = stackalloc int[1000];
+
+            //Sample the original array
+            for (int i = 0; i < copyArray.Length; ++i)
+            {
+                copyArray[i] = inputArray[i];
+            }
+
+            //Do operations on the sampled array
+            for (int i = 0; i < copyArray.Length; ++i)
+            {
+                ++copyArray[i];
+            }
+
+            //Use the modifications
+            for (int i = 0; i < copyArray.Length; ++i)
+            {
+                inputArray[i] = copyArray[i];
+            }
+
+
+        }
+    }
+}
